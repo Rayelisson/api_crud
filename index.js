3cf9853af05fac9cbd32ccde7e4e8bb4de12808e
rayelisson <33360322+Rayelisson@users.noreply.github.com>
2024-02-20 10:19:20 -0300

    tentado do  conserta o json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
-import swaggerRouter from "./src/routes/swagger.route.cjs";
+import swaggerRouter from "./src/routes/swagger.route.js";
 
 import newsRoute from "./src/routes/news.route.js";
 
