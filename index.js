363bb45203571985373f67a5be9aedd6ae7a68f2
rayelisson <33360322+Rayelisson@users.noreply.github.com>
2024-02-19 23:06:43 -0300

    parte env

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ import dotenv from "dotenv";
 
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
+import swaggerRouter from "./src/routes/swagger.route.js";
+
+import newsRoute from "./src/routes/news.route.js";
 
 dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,5 +18,7 @@ connectDatabase();
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
+app.use("/news", newsRoute);
+app.use("/doc", swaggerRouter);
 
 app.listen(3000, () => console.log(`Servidor rodando na porta ${port}`));
